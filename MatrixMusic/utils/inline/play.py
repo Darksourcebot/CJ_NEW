1549bf5768acd6889926dd4928a29e7b36ca756c
DEV-E3DAM <139201770+Darksourcebot@users.noreply.github.com>
2024-05-05 23:15:35 +0300

    Update play.py

--- MatrixMusic/utils/inline/play.py
+++ MatrixMusic/utils/inline/play.py
@@ -84,7 +84,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="ᏢᎪႮՏᎬ", callback_data=f"ADMIN Pause|{chat_id}"),
             
         ],[
-            InlineKeyboardButton(text="𝙎𝙊𝙐𝙍𝘾𝙀 𝗖𝗝", url=f"https://t.me/CG_G11"),
+            InlineKeyboardButton(text="𝐂𝐇𝐀𝐍𝐍𝐄𝐋", url=f"https://t.me/CG_G11"),
             InlineKeyboardButton(text="ᏀᎡΌႮᏢ", url=f"https://t.me/PO_UV"),
         ],[
             InlineKeyboardButton(text="『 َِٓ𝗘َِ𝟯َِٓ𝗗َِٓ𝗔َِٓ𝗠َِٓ ℡ 』 ☬ ➥ مٓمٓــۄل ||", url=f"https://t.me/DAD_E3DAM"),
