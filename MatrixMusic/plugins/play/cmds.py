251b138b55781283c0a6137f56b1a41af2b7f2ac
DEV-E3DAM <139201770+Darksourcebot@users.noreply.github.com>
2024-05-05 23:40:57 +0300

    Update cmds.py

--- MatrixMusic/plugins/play/cmds.py
+++ MatrixMusic/plugins/play/cmds.py
@@ -43,7 +43,7 @@ async def zdatsr(client: Client, message: Message):
          
 
 @app.on_callback_query(filters.regex("zzzdv"))
-async def mpdtsf(_, query: CallbackQuery):
+async def mpdtsf(query: CallbackQuery):
    await query.edit_message_text(
        f"""<b>↯︙مرحباً بك عزيزي المطور </b>\n\n<b>↯︙استخدم الازرار بالاسفل\n↯︙ل تصفح اوامر الميوزك</b>""",
         reply_markup=InlineKeyboardMarkup(
